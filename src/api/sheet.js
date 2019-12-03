0a76b112df7adef5fd58a69be4caa64223ec003f
ngochangjelly <ngochang29397@gmail.com>
2019-12-03 10:07:07 +0700

    add input search box

--- src/api/sheet.js
+++ src/api/sheet.js
@@ -1,6 +1,6 @@
 import config from './config';
 
-const load = callback => {
+export const load = callback => {
   window.gapi.client.load('sheets', 'v4', () => {
     window.gapi.client.sheets.spreadsheets.values
       .get({
@@ -26,4 +26,30 @@ const load = callback => {
       );
   });
 };
-export default load;
+export const makeApiCall = () => {
+  var params = {
+    // The spreadsheet to request.
+    spreadsheetId: config.spreadsheetId,
+
+    // The ranges to retrieve from the spreadsheet.
+    ranges: [], // TODO: Update placeholder value.
+
+    // True if grid data should be returned.
+    // This parameter is ignored if a field mask was set in the request.
+    includeGridData: false // TODO: Update placeholder value.
+  };
+
+  var request = window.gapi.client.sheets.spreadsheets.get({
+    spreadsheetId: params.spreadsheetId,
+    range: 'Sheet1!A1:C2'
+  });
+  request.then(
+    function(response) {
+      // TODO: Change code below to process the `response` object:
+      console.log(response.result);
+    },
+    function(reason) {
+      console.error('error: ' + reason.result.error.message);
+    }
+  );
+};
